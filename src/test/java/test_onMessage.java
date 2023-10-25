aa17b572bd9a06ebc53df3a52344a8409d371de1
Kloping <3474006766@qq.com>
2023-10-25 12:01:28 +0800

    update test

--- src/test/java/test_onMessage.java
+++ src/test/java/test_onMessage.java
@@ -1,6 +1,13 @@
 import io.github.kloping.qqbot.Starter;
+import io.github.kloping.qqbot.api.message.MessageChannelReceiveEvent;
 import io.github.kloping.qqbot.api.message.MessageDirectReceiveEvent;
 import io.github.kloping.qqbot.api.message.MessageEvent;
+import io.github.kloping.qqbot.entities.ex.At;
+import io.github.kloping.qqbot.entities.ex.AtAll;
+import io.github.kloping.qqbot.entities.ex.Image;
+import io.github.kloping.qqbot.entities.ex.PlainText;
+import io.github.kloping.qqbot.entities.ex.msg.MessageChain;
+import io.github.kloping.qqbot.entities.qqpd.data.Emoji;
 import io.github.kloping.qqbot.entities.qqpd.message.RawMessage;
 import io.github.kloping.qqbot.impl.EventReceiver;
 import io.github.kloping.qqbot.impl.ListenerHost;
@@ -38,6 +45,25 @@ public class test_onMessage {
             @EventReceiver
             public void onEvent(MessageDirectReceiveEvent event) {
                 event.send("测试通过");
+            }
+
+            @EventReceiver
+            public void onMessage(MessageChannelReceiveEvent event) {
+                //消息类型遍历
+                MessageChain chain = event.getMessage();
+                chain.forEach((e) -> {
+                    if (e instanceof Emoji) {
+                        Emoji emoji = (Emoji) e;
+                    } else if (e instanceof At) {
+                        At at = (At) e;
+                    } else if (e instanceof AtAll) {
+                        AtAll atAll = (AtAll) e;
+                    } else if (e instanceof Image) {
+                        Image image = (Image) e;
+                    } else if (e instanceof PlainText) {
+                        PlainText plainText = (PlainText) e;
+                    }
+                });
             }
         });
     }
