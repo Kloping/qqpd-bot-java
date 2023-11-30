023ff806482e5b5c5a374943a4a1dea614105abc
Kloping <3474006766@qq.com>
2023-11-30 13:12:40 +0800

    action test

---
+++ src/test/java/MvnTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @author github.kloping
+ */
+public class MvnTest {
+    public MvnTest() {
+        System.out.println("开始测试");
+    }
+
+    @Test
+    public void test() {
+        System.out.println("测试成功");
+    }
+}
