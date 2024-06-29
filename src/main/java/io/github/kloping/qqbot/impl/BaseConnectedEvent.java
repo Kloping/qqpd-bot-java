86f50eb220929b9581dbdd996cdf7dc2d2883a6b
Kloping <3474006766@qq.com>
2024-06-29 14:50:05 +0800

    fix version and update

--- src/main/java/io/github/kloping/qqbot/impl/BaseConnectedEvent.java
+++ src/main/java/io/github/kloping/qqbot/impl/BaseConnectedEvent.java
@@ -41,6 +41,6 @@ public class BaseConnectedEvent implements ConnectedEvent {
 
     @Override
     public String toString() {
-        return String.format("Bot(%s) Connected! By author kloping of bot-qqpd-java for version 1.5.1-R2", bot.getConfig().getAppid());
+        return String.format("Bot(%s) Connected! By author kloping of bot-qqpd-java for version 1.5.1-Beta3", bot.getConfig().getAppid());
     }
 }
